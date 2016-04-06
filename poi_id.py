9854cfb9de8f66f5e0ab92c37c98a8146f6ee891
joash xu <joash.xu@gmail.com>
2016-04-06 19:23:02 +0700

    Update POI and classifier file.

--- poi_id.py
+++ poi_id.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
+
 import pandas as pd
 
 from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import StratifiedShuffleSplit
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
 
 from tools.feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
@@ -16,21 +18,15 @@ from utils import get_original_data, get_k_best_features
 data = get_original_data()
 df = pd.DataFrame.from_dict(data).transpose()
 
-# Convert some fields to float
-float_columns = ['bonus', 'deferral_payments', 'deferred_income', 'director_fees',
- 'exercised_stock_options', 'expenses', 'from_messages', 'from_poi_to_this_person',
- 'from_this_person_to_poi', 'loan_advances', 'long_term_incentive', 'other',
- 'restricted_stock', 'restricted_stock_deferred', 'salary', 'shared_receipt_with_poi',
- 'to_messages', 'total_payments', 'total_stock_value']
-
-df[float_columns] = df[float_columns].astype('float')
+# Some field need to be converted as numeric type
+df = df.convert_objects(convert_numeric=True)
 
 # Task 1: Select what features you'll use.
 # -----------------------------------------------------------------------------------------
 
 initial_feature_list = ['bonus', 'exercised_stock_options', 'expenses',
                         'from_messages', 'from_poi_to_this_person',
-                        'from_this_person_to_poi',  'long_term_incentive', 'other',
+                        'from_this_person_to_poi', 'long_term_incentive', 'other',
                         'restricted_stock', 'salary', 'shared_receipt_with_poi',
                         'to_messages', 'total_payments', 'total_stock_value']
 
@@ -52,9 +48,9 @@ df.drop(df.index[df.index == 'LOCKHART EUGENE E'], inplace=True)
 # -----------------------------------------------------------------------------------------
 
 # Fraction from POI
-df['fraction_from_poi'] = df.apply(lambda x:x['from_poi_to_this_person'] / x['to_messages'], axis=1)
+df['fraction_from_poi'] = df.apply(lambda x: x['from_poi_to_this_person'] / x['to_messages'], axis=1)
 # Fraction to POI
-df['fraction_to_poi'] = df.apply(lambda x:x['from_this_person_to_poi'] / x['from_messages'], axis=1)
+df['fraction_to_poi'] = df.apply(lambda x: x['from_this_person_to_poi'] / x['from_messages'], axis=1)
 
 initial_feature_list += ['fraction_from_poi', 'fraction_to_poi']
 
@@ -65,7 +61,7 @@ my_dataset = df.fillna('NaN').transpose().to_dict()
 # -----------------------------------------------------------------------------------------
 
 # Let's reduce the features to 4 for now.
-kbest_features = get_k_best_features(my_dataset, ['poi'] + initial_feature_list, 4)
+kbest_features = get_k_best_features(my_dataset, ['poi'] + initial_feature_list)
 kbest_features = [item[0] for item in kbest_features[:4]]
 
 # The first feature must be "poi".
@@ -80,18 +76,24 @@ labels, features = targetFeatureSplit(data)
 
 # We will be using GridSearchCV to find the optimum
 # parameters.
+pipe = Pipeline([
+    ('scaler', MinMaxScaler()),
+    ('classifier', KNeighborsClassifier())
+])
 
 # Parameters to be tuned
 metrics = ['minkowski', 'euclidean', 'manhattan']
 weights = ['uniform', 'distance']
 n_neighbors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-param_grid_knc = dict(metric=metrics, weights=weights, n_neighbors=n_neighbors)
+param_grid_knc = dict(classifier__metric=metrics,
+                      classifier__weights=weights,
+                      classifier__n_neighbors=n_neighbors)
 
 # Setup cross validation
-cv = StratifiedShuffleSplit(labels, 1000, random_state=17)
+cv = StratifiedShuffleSplit(labels, 1000, random_state=42)
 
 # Run the GridSearch
-clf_knc = GridSearchCV(KNeighborsClassifier(), param_grid=param_grid_knc, cv=cv)
+clf_knc = GridSearchCV(pipe, param_grid=param_grid_knc, cv=cv, scoring='recall')
 clf_knc.fit(features, labels)
 
 clf = clf_knc.best_estimator_
@@ -99,8 +101,5 @@ clf = clf_knc.best_estimator_
 
 # Task 6: Check result
 # -----------------------------------------------------------------------------------------
-
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
 
 dump_classifier_and_data(clf, my_dataset, features_list)
